262658247982bf47e8e10032b09746bf297db9cc
Rafael Andre Alves de Siqueira <r243360@dac.unicamp.br>
2023-03-29 19:22:22 -0300

    commented lab03

--- Lab03/dec2_to_4.vhd
+++ Lab03/dec2_to_4.vhd
@@ -1,6 +1,16 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+--Esta funçao logica gera 4 saidas a partir de en, w1 e w0
+-- cada yi sera uma combinacao de w0 e w1,
+-- sendo y0 = not(w0) * not(w1) * en
+-- pois 0 = 0b00 considerando w0
+-- associado ao bit mais significativo
+-- e w1 ao menos.
+-- en faz parte do produto pois este resultado so sera
+-- transmitido caso enable = 1. Caso contrario,
+-- o resultado sera 0.
+
 entity dec2_to_4 is
   port(en, w1, w0: in std_logic;
        y3, y2, y1, y0: out std_logic);
