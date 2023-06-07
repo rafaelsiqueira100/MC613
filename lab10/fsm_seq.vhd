7fcace96dc0ffe57aea801ee73de903cba64a32d
Rafael Andre Alves de Siqueira <r243360@dac.unicamp.br>
2023-06-07 14:24:32 -0300

    lab10 tested

--- lab10/fsm_seq.vhd
+++ lab10/fsm_seq.vhd
@@ -7,3 +7,60 @@
 -- entao, chegar no estado C alimentado o sistema com 1 e depois 0
 -- e alimentar com o inverso da sequencia. Isto fara a fsm chegar no estado B cuja saida eh
 -- 1 e assim sera o input do da saida Z do fsm_seq
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity fsm_seq is
+  port (
+      clock : in  std_logic;
+      reset : in  std_logic;
+      w     : in  std_logic;
+      z     : out std_logic
+    );
+end fsm_seq;
+architecture arch of fsm_seq is
+	type type_state is (a, b, c, d, e);
+	signal current_state: type_state;
+begin
+	process(clock)
+	begin
+		if(clock'event and clock = '1') then
+			if (reset = '1') then
+				current_state <= a;
+			else
+				if current_state = a then
+					if w = '0' then
+						current_state <= b;--0
+					else
+						current_state <= a;--1
+					end if;
+				elsif current_state = b then
+					if w = '1' then
+							current_state <= c;--01
+					else
+							current_state <= b;--0
+					end if;
+				elsif current_state = c then
+					if w = '0' then
+							current_state <= d;--010
+					else
+							current_state <= a;--1
+					end if;
+				elsif current_state = d then
+					if w = '1' then
+							current_state <= e;--0101
+					else
+							current_state <= b;--0
+					end if;
+				elsif current_state = e then
+					if w = '0' then
+							current_state <= d;--01010
+					else
+							current_state <= a;--01011
+					end if;
+				end if;
+			end if;
+		end if;
+	end process;
+		z <= '1' when E = current_state else '0';
+end arch;
---
+++ lab10/output_files/lab10.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 17.1.0 Build 590 10/25/2017 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+	P ActionCode(Cfg)
+		Device PartName(5CGXFC7C7F23) Path("/home/ec2019-ceb/ra243360/Downloads/MC613/MC613/lab10/output_files/") File("lab10.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
